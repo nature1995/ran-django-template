265507c3be6105607b2fcfba8e00235390811905
nature1995 <gzr2008@vip.qq.com>
2019-04-13 13:49:27 -0400

    Add test case

--- apps/blog/tests.py
+++ apps/blog/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
-
+from apps.blog.models import *
 # Create your tests here.
+
+
+class BlogTestCase(TestCase):
+    def setUp(self):
+        Tag.objects.create(name="label1")
+        Tag.objects.create(name="label2")
+        Category.objects.create(name="catecory1")
+        Category.objects.create(name="catecory2")
+        Article.objects.create(title="This is article1 title", content="This is article1 content",
+                               category=Category.objects.get(name="catecory1"), pub_time=now())
+        Article.objects.create(title="This is article2 title", content="This is article2 content",
+                               category=Category.objects.get(name="catecory2"), pub_time=now())
+
+    def test_tag_create(self):
+        label1 = Tag.objects.get(name="label1")
+        label2 = Tag.objects.get(name="label2")
+        self.assertEqual(label1.name, 'label1')
+        self.assertEqual(label2.name, 'label2')
+
+    def test_catecory_create(self):
+        catecory1 = Category.objects.get(name="catecory1")
+        catecory2 = Category.objects.get(name="catecory2")
+        self.assertEqual(catecory1.name, 'catecory1')
+        self.assertEqual(catecory2.name, 'catecory2')
+
+    def test_article_create(self):
+        Article1 = Article.objects.get(title="This is article1 title")
+        Article2 = Article.objects.get(title="This is article2 title")
+        self.assertEqual(Article1.title, 'This is article1 title')
+        self.assertEqual(Article2.title, 'This is article2 title')
+        self.assertEqual(Article1.content, 'This is article1 content')
+        self.assertEqual(Article2.content, 'This is article2 content')
+        self.assertIn(Article1.category.name, 'catecory1')
+        self.assertIn(Article2.category.name, 'catecory2')
--- apps/gallery/tests.py
+++ apps/gallery/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
-
+from apps.gallery.models import *
 # Create your tests here.
+
+
+class ModelTestCase(TestCase):
+    def setUp(self):
+        Gallery.objects.create(title="gallery1 title", description="gallery1 description")
+        Gallery.objects.create(title="gallery2 title", description="gallery2 description")
+
+    def test_gallery_create(self):
+        gallery1 = Gallery.objects.get(title="gallery1 title")
+        gallery2 = Gallery.objects.get(title="gallery2 title")
+        self.assertEqual(gallery1.description, 'gallery1 description')
+        self.assertEqual(gallery2.description, 'gallery2 description')
+        self.assertEqual(gallery1.title, 'gallery1 title')
+        self.assertEqual(gallery2.title, 'gallery2 title')
