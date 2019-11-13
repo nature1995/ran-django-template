51248fd78dc36208ee67a35ffbb718de81f3d10c
Ziran Gong <gzr2008@vip.qq.com>
2019-11-13 12:34:51 -0500

    feat:加入url，删除collection生成的文件

--- apps/gallery/models.py
+++ apps/gallery/models.py
@@ -1,14 +1,23 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
 
 
 class Gallery(models.Model):
-    description = models.CharField(default='填写简介', max_length=100)
-    title = models.CharField(default='作品标题', max_length=50)
-    image = models.ImageField(default='default.png',upload_to='images/')
+    title = models.CharField(default='项目标题', max_length=100)
+    description = models.CharField(default='项目简介', max_length=300)
+    image = models.ImageField(default='default.png', upload_to='images/')
+    url = models.CharField(default="项目链接", max_length=100)
+    created_time = models.DateTimeField(verbose_name='创建时间', default=now)
+    last_mod_time = models.DateTimeField(verbose_name='修改时间', default=now)
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        ordering = ['last_mod_time']
+        verbose_name = '项目名称'  # 指定后台显示模型名称
+        verbose_name_plural = '项目列表'  # 指定后台显示模型复数名称
+        db_table = "gallery"  # 数据库表名
 
